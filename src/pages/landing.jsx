4a7f4e4ba32af0332b49353192982aea4565744a
Timothy Daniel-Paul <timothydanielpaul@gmail.com>
2024-08-11 17:56:52 +0100

    Changes

--- src/pages/landing.jsx
+++ src/pages/landing.jsx
@@ -1,7 +1,7 @@
-import {Button} from "@/components/ui/button";
-import {Input} from "@/components/ui/input";
-import {useState} from "react";
-import {useNavigate} from "react-router-dom";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import {
   Accordion,
   AccordionContent,
@@ -20,7 +20,7 @@ const LandingPage = () => {
 
   return (
     <div className="flex flex-col items-center">
-      <h2 className="my-10 sm:my-16 text-3xl sm:text-6xl lg:text-7xl text-white text-center font-extrabold">
+      <h2 className="my-10 sm:my-16 text-3xl sm:text-6xl lg:text-7xl text-black text-center font-extrabold">
         The only URL Shortener <br /> you&rsquo;ll ever need! 👇
       </h2>
       <form
